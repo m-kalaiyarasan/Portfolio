b12c834985e575f6468501db2326cec10319c1b6
kalaiyarasan.offl.2004@gmail.com <kalaiyarasan.offl.2004@gmail.com>
2024-10-19 13:09:28 +0000

    mobile edit

--- Components/home_baner.php
+++ Components/home_baner.php
@@ -4,9 +4,9 @@
 				<div class="row">
 					<div class="col-lg-7">
 						<div class="banner_content">
-							<h3 class="text-uppercase">Hell0</h3>
-							<h2 class="text-uppercase kalai">Iam Kalaiyarasan</h2>
-							<h5 class="text-uppercase">Offencive and Defensive Programmer</h5>
+							<h3 class="text-uppercase">Hello,</h3>
+							<h2 class="text-uppercase kalai">Kalaiyarasan M</h2>
+							<h5 class="kalai2 text-uppercase">Offencive and Defensive Programmer</h5>
 							<div class="d-flex align-items-center">
 								<a class="primary_btn" href="contact.php"><span>Contact</span></a>
 								<a class="primary_btn tr-bg" href="Resume-kalaiyarasan.pdf" download><span>Get CV</span></a>
